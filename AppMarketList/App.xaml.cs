f8e8ab038829c1706e1e52ce9ade106c75054365
MathFerreiraDev <Usuario@CPD02-M11>
2024-03-04 12:24:07 -0300

    Impementação de CRUD e inicio de construção de interface

--- AppMarketList/App.xaml.cs
+++ AppMarketList/App.xaml.cs
@@ -1,7 +1,22 @@
-﻿namespace AppMarketList
+﻿using AppMarketList.Helpers;
+
+namespace AppMarketList
 {
     public partial class App : Application
     {
+        public static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db { 
+            get { 
+                if (_db == null) {
+                    string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                } 
+                return _db;
+            } 
+        }
+
         public App()
         {
             InitializeComponent();
--- AppMarketList/Helpers/SQLiteDatabaseHelper.cs
+++ AppMarketList/Helpers/SQLiteDatabaseHelper.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AppMarketList.Models;
 using SQLite;
-using AppMarketList.Models;
 
 
 namespace AppMarketList.Helpers
@@ -22,6 +17,32 @@ namespace AppMarketList.Helpers
         public Task<int> Insert(Produto p)
         {
             return _conn.InsertAsync(p);
+        }
+
+        public Task<List<Produto>> Update(Produto p)
+        {
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
+
+            return _conn.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
+        }
+
+        public Task<List<Produto>> SelectAll()
+        {
+
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<int> Delete(int id)
+        {
+
+            return _conn.Table<Produto>().DeleteAsync( i => i.Id == id);
+        }
+
+        public Task<List<Produto>> SearchProduct(string p)
+        {
+            string sql = $"SELECT * FROM Produto WHERE descricao LIKE '%{p}%'";
+
+            return _conn.QueryAsync<Produto>(sql);
         }
     }
 }
