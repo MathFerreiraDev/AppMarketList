40437fdf0c2330de7a417ec01ed2534a96be1d34
Usuario <Usuario@CPD01-M01>
2024-04-04 14:17:27 -0300

    Alteração em grid

--- AppMarketList/MainPage.xaml.cs
+++ AppMarketList/MainPage.xaml.cs
@@ -15,29 +15,29 @@ namespace AppMarketList
 
         private void btn_Somar_Clicked(object sender, EventArgs e)
         {
-            double somador = list_products.Sum(i=> (i.Preco * i.Quantidade) );
+            double somador = list_products.Sum(i => (i.Preco * i.Quantidade));
             string msg = $"Soma total em itens: {somador:C}";
-            DisplayAlert("Resultado",$"A somatória de sua lista é equivalente a: {somador:C}", "Concluído");
+            DisplayAlert("Resultado", $"A somatória de sua lista é equivalente a: {somador:C}", "Concluído");
         }
 
         private async void btn_Add_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new Views.NovoProduto());
 
-            
+
         }
 
         protected async override void OnAppearing()
         {
-           if(list_products.Count == 0)
+            if (list_products.Count == 0)
             {
-              
-                    List<Produto> tmp = await App.Db.SelectAll();
-                    foreach (Produto p in tmp)
-                    {
-                        list_products.Add(p);
-                    }
-                
+
+                List<Produto> tmp = await App.Db.SelectAll();
+                foreach (Produto p in tmp)
+                {
+                    list_products.Add(p);
+                }
+
             }
         }
 
@@ -45,18 +45,18 @@ namespace AppMarketList
         {
             string q = e.NewTextValue;
             list_products.Clear();
-          
-                List<Produto> tmp = await App.Db.SearchProduct(q);
-                foreach (Produto p in tmp)
-                {
-                    list_products.Add(p);
-                }
-            
+
+            List<Produto> tmp = await App.Db.SearchProduct(q);
+            foreach (Produto p in tmp)
+            {
+                list_products.Add(p);
+            }
+
         }
 
         private void ref_carregando_Refreshing(object sender, EventArgs e)
         {
-            list_products.Clear();
+            /*list_products.Clear();
             Task.Run(async () =>
             {
                 List<Produto> tmp = await App.Db.SelectAll();
@@ -65,7 +65,7 @@ namespace AppMarketList
                     list_products.Add(p);
                 }
             });
-            ref_carregando.IsRefreshing = false;
+            ref_carregando.IsRefreshing = false;*/
         }
 
         private async void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
@@ -86,13 +86,14 @@ namespace AppMarketList
                 Produto p = selected.BindingContext as Produto;
                 bool confirm = await DisplayAlert("Tem certeza?", $"Remover produto {p.Descricao}", "Confirmar", "Cancelar");
 
-                if(confirm)
+                if (confirm)
                 {
                     await App.Db.Delete(p.Id);
                     await DisplayAlert("Sucesso", $"O item {p.Descricao} foi removido da lista!", "OK");
                     list_products.Remove(p);
                 }
-            }catch (Exception ex)
+            }
+            catch (Exception ex)
             {
                 await DisplayAlert("Ops", ex.Message, "OK");
             }
